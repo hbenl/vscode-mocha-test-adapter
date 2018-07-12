0b927a585a72d8c3fa21ea81efdeaa19d2ccd045
Holger Benl <hbenl@evandor.de>
2018-07-12 14:52:03 +0200

    use the Registrar from the util package

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import { TestExplorerExtension, testExplorerExtensionId } from 'vscode-test-adapter-api';
 import { MochaAdapter } from './adapter';
-import { Log } from 'vscode-test-adapter-util';
+import { Log, TestAdapterRegistrar } from 'vscode-test-adapter-util';
 
 export async function activate(context: vscode.ExtensionContext) {
 
@@ -18,68 +18,10 @@ export async function activate(context: vscode.ExtensionContext) {
 			await testExplorerExtension.activate();
 		}
 
-		context.subscriptions.push(new TestAdapterRegistrar(testExplorerExtension.exports, log));
-	}
-}
-
-class TestAdapterRegistrar {
-
-	private readonly registeredAdapters = new Map<vscode.WorkspaceFolder, MochaAdapter>();
-
-	constructor(
-		private readonly testExplorer: TestExplorerExtension,
-		private readonly log: Log
-	) {
-
-		if (vscode.workspace.workspaceFolders) {
-			for (const workspaceFolder of vscode.workspace.workspaceFolders) {
-				this.add(workspaceFolder);
-			}
-		}
-
-		log.info('Initialization finished');
-
-		vscode.workspace.onDidChangeWorkspaceFolders((event) => {
-
-			for (const workspaceFolder of event.removed) {
-				this.remove(workspaceFolder);
-			}
-
-			for (const workspaceFolder of event.added) {
-				this.add(workspaceFolder);
-			}
-		});
-	}
-
-	add(workspaceFolder: vscode.WorkspaceFolder) {
-
-		if (this.log.enabled) this.log.info(`Creating adapter for ${workspaceFolder.uri.fsPath}`);
-
-		const adapter = new MochaAdapter(workspaceFolder, this.log);
-		this.registeredAdapters.set(workspaceFolder, adapter);
-
-		if (this.log.enabled) this.log.info(`Registering adapter for ${workspaceFolder.uri.fsPath}`);
-
-		this.testExplorer.registerAdapter(adapter);
-	}
-
-	remove(workspaceFolder: vscode.WorkspaceFolder) {
-
-		const adapter = this.registeredAdapters.get(workspaceFolder);
-		if (adapter) {
-
-			if (this.log.enabled) this.log.info(`Removing adapter for ${workspaceFolder.uri.fsPath}`);
-
-			this.testExplorer.unregisterAdapter(adapter);
-			this.registeredAdapters.delete(workspaceFolder);
-			adapter.dispose();
-		}
-	}
-
-	dispose(): void {
-		for (const workspaceFolder of this.registeredAdapters.keys()) {
-			this.remove(workspaceFolder);
-		}
-		this.log.dispose();
+		context.subscriptions.push(new TestAdapterRegistrar(
+			testExplorerExtension.exports,
+			(workspaceFolder) => new MochaAdapter(workspaceFolder, log),
+			log
+		));
 	}
 }
