4d702af741169e6223dabe11e7bfc959fd686d26
Holger Benl <hbenl@evandor.de>
2019-03-03 15:32:11 +0100

    add tests for loading and running bdd and tdd tests

---
+++ src/test/expectedTests.ts
@@ -0,0 +1,226 @@
+import * as path from 'path';
+import { TestSuiteInfo, TestRunStartedEvent, TestRunFinishedEvent, TestSuiteEvent, TestEvent } from "vscode-test-adapter-api";
+
+export function getExpectedTests(workspaceName: string): TestSuiteInfo {
+
+	const workspaceFolderPath = path.resolve(__dirname, './workspaces/' + workspaceName);
+	const staticTestFilePath = path.join(workspaceFolderPath, 'test/static.js');
+	const dynamicTestFilePath = path.join(workspaceFolderPath, 'test/dynamic.js');
+
+	return {
+		"type": "suite",
+		"id": workspaceFolderPath + ": Mocha",
+		"label": "Mocha",
+		"children": [
+			{
+				"type": "suite",
+				"id": dynamicTestFilePath + ": Suite #3",
+				"label": "Suite #3",
+				"file": dynamicTestFilePath,
+				"line": 0,
+				"children": [
+					{
+						"type": "test",
+						"id": dynamicTestFilePath + ": Suite #3 Test #3.1",
+						"label": "Test #3.1",
+						"file": dynamicTestFilePath,
+						"line": 1,
+						"skipped": false
+					},
+					{
+						"type": "test",
+						"id": dynamicTestFilePath + ": Suite #3 Test #3.2",
+						"label": "Test #3.2",
+						"file": dynamicTestFilePath,
+						"line": 3,
+						"skipped": false
+					},
+					{
+						"type": "test",
+						"id": dynamicTestFilePath + ": Suite #3 Test #3.3",
+						"label": "Test #3.3",
+						"file": dynamicTestFilePath,
+						"line": 3,
+						"skipped": false
+					}
+				]
+			},
+			{
+				"type": "suite",
+				"id": staticTestFilePath + ": Suite #1",
+				"label": "Suite #1",
+				"file": staticTestFilePath,
+				"line": 2,
+				"children": [
+					{
+						"type": "test",
+						"id": staticTestFilePath + ": Suite #1 Test #1.1",
+						"label": "Test #1.1",
+						"file": staticTestFilePath,
+						"line": 4,
+						"skipped": false
+					},
+					{
+						"type": "test",
+						"id": staticTestFilePath + ": Suite #1 Test #1.2",
+						"label": "Test #1.2",
+						"file": staticTestFilePath,
+						"line": 8,
+						"skipped": false
+					},
+					{
+						"type": "test",
+						"id": staticTestFilePath + ": Suite #1 Test #1.3",
+						"label": "Test #1.3",
+						"file": staticTestFilePath,
+						"line": 12,
+						"skipped": true
+					}
+				]
+			},
+			{
+				"type": "suite",
+				"id": staticTestFilePath + ": Suite #2",
+				"label": "Suite #2",
+				"file": staticTestFilePath,
+				"line": 16,
+				"children": [
+					{
+						"type": "test",
+						"id": staticTestFilePath + ": Suite #2 Test #2.1",
+						"label": "Test #2.1",
+						"file": staticTestFilePath,
+						"line": 17,
+						"skipped": true
+					}
+				]
+			}
+		]
+	}
+}
+
+export function getExpectedTestRunEvents(workspaceName: string): (TestRunStartedEvent | TestRunFinishedEvent | TestEvent | TestSuiteEvent)[] {
+
+	const workspaceFolderPath = path.resolve(__dirname, './workspaces/' + workspaceName);
+	const staticTestFilePath = path.join(workspaceFolderPath, 'test/static.js');
+	const dynamicTestFilePath = path.join(workspaceFolderPath, 'test/dynamic.js');
+
+	return [
+		{
+			"type": "started",
+			"tests": [
+				workspaceFolderPath + ": Mocha"
+			]
+		},
+		{
+			"type": "suite",
+			"suite": "undefined: ",
+			"state": "running"
+		},
+		{
+			"type": "suite",
+			"suite": dynamicTestFilePath + ": Suite #3",
+			"state": "running"
+		},
+		{
+			"type": "test",
+			"test": dynamicTestFilePath + ": Suite #3 Test #3.1",
+			"state": "running"
+		},
+		{
+			"type": "test",
+			"test": dynamicTestFilePath + ": Suite #3 Test #3.1",
+			"state": "passed"
+		},
+		{
+			"type": "test",
+			"test": dynamicTestFilePath + ": Suite #3 Test #3.2",
+			"state": "running"
+		},
+		{
+			"type": "test",
+			"test": dynamicTestFilePath + ": Suite #3 Test #3.2",
+			"state": "passed"
+		},
+		{
+			"type": "test",
+			"test": dynamicTestFilePath + ": Suite #3 Test #3.3",
+			"state": "running"
+		},
+		{
+			"type": "test",
+			"test": dynamicTestFilePath + ": Suite #3 Test #3.3",
+			"state": "passed"
+		},
+		{
+			"type": "suite",
+			"suite": dynamicTestFilePath + ": Suite #3",
+			"state": "completed"
+		},
+		{
+			"type": "suite",
+			"suite": staticTestFilePath + ": Suite #1",
+			"state": "running"
+		},
+		{
+			"type": "test",
+			"test": staticTestFilePath + ": Suite #1 Test #1.1",
+			"state": "running"
+		},
+		{
+			"type": "test",
+			"test": staticTestFilePath + ": Suite #1 Test #1.1",
+			"state": "passed"
+		},
+		{
+			"type": "test",
+			"test": staticTestFilePath + ": Suite #1 Test #1.2",
+			"state": "running"
+		},
+		{
+			"type": "test",
+			"test": staticTestFilePath + ": Suite #1 Test #1.2",
+			"state": "failed",
+			"decorations": [
+				{
+					"line": 9,
+					"message": "1 == 2"
+				}
+			],
+			"message": "AssertionError [ERR_ASSERTION]: 1 == 2\n    at Context.<anonymous> (test/static.js:10:10)\n\n+ expected - actual\n\n-1\n+2\n"
+		},
+		{
+			"type": "test",
+			"test": staticTestFilePath + ": Suite #1 Test #1.3",
+			"state": "skipped"
+		},
+		{
+			"type": "suite",
+			"suite": staticTestFilePath + ": Suite #1",
+			"state": "completed"
+		},
+		{
+			"type": "suite",
+			"suite": staticTestFilePath + ": Suite #2",
+			"state": "running"
+		},
+		{
+			"type": "test",
+			"test": staticTestFilePath + ": Suite #2 Test #2.1",
+			"state": "skipped"
+		},
+		{
+			"type": "suite",
+			"suite": staticTestFilePath + ": Suite #2",
+			"state": "completed"
+		},
+		{
+			"type": "suite",
+			"suite": "undefined: ",
+			"state": "completed"
+		},
+		{
+			"type": "finished"
+		}
+	];
+}
---
+++ src/test/loadAndRun.test.ts
@@ -0,0 +1,39 @@
+import * as assert from 'assert';
+import { createTestMochaAdapter } from './adapter';
+import { getExpectedTests, getExpectedTestRunEvents } from './expectedTests';
+
+describe("Loading tests", function() {
+
+	for (const ui of [ 'bdd', 'tdd' ]) {
+
+		it(`should work with ${ui} tests`, async function() {
+
+			const workspaceFolderName = `javascript/${ui}`;
+			const adapter = await createTestMochaAdapter(workspaceFolderName);
+	
+			await adapter.load();
+	
+			const rootSuite = adapter.getLoadedTests();
+
+			assert.deepStrictEqual(rootSuite, getExpectedTests(workspaceFolderName));
+		});
+	}
+});
+
+describe("Running tests", function() {
+
+	for (const ui of [ 'bdd', 'tdd' ]) {
+
+		it(`should work with ${ui} tests`, async function() {
+
+			const workspaceFolderName = `javascript/${ui}`;
+			const adapter = await createTestMochaAdapter(workspaceFolderName);
+	
+			await adapter.load();
+			const rootSuite = adapter.getLoadedTests();
+			await adapter.run([ rootSuite!.id ]);
+
+			assert.deepStrictEqual(adapter.getTestRunEvents(), getExpectedTestRunEvents(workspaceFolderName));
+		});
+	}
+});
