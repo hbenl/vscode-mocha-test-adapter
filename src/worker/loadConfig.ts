aa7ec47884ffb8c82885ee7c1942bbecd6596056
Manuel Thalmann <m@nuth.ch>
2023-04-24 17:31:23 +0200

    Enable support for the `extends` config
    This commit fixes #223

--- src/worker/loadConfig.ts
+++ src/worker/loadConfig.ts
@@ -1,3 +1,12 @@
 import { loadOptions } from 'mocha/lib/cli/options';
+import * as yargs from "yargs";
 
-process.send!(loadOptions(process.argv.slice(2)));
+let args: yargs.Arguments = loadOptions(process.argv.slice(2)) as any;
+
+(
+	async () => {
+		process.send!(
+			yargs.parserConfiguration(
+				require("mocha/lib/cli/options").YARGS_PARSER_CONFIG ?? {}).config(
+					args).parse(args._ as any));
+	})();
